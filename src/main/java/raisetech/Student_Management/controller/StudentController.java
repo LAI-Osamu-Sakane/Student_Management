f782e4f85bd5836979d036f17a8a6f892811b29e
LAI-Osamu-Sakane <sakane.osamu@link-academy.co.jp>
2024-08-20 17:31:28 +0900

    12th 課題終了まで

--- src/main/java/raisetech/Student_Management/controller/StudentController.java
+++ src/main/java/raisetech/Student_Management/controller/StudentController.java
@@ -1,30 +1,41 @@
 package raisetech.Student_Management.controller;
 
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import raisetech.Student_Management.controller.converter.StudentConverter;
 import raisetech.Student_Management.data.Student;
 import raisetech.Student_Management.data.StudentsCourses;
+import raisetech.Student_Management.domain.StudentDetail;
 import raisetech.Student_Management.service.StudentService;
 
 @RestController
 public class StudentController {
 
   private StudentService service;
+  private StudentConverter converter;
 
   @Autowired
-  public StudentController(StudentService service) {
+  public StudentController(StudentService service, StudentConverter converter ) {
     this.service = service;
+    this.converter = converter;
   }
 
   @GetMapping("/studentList")
-  public List<Student> getStudentList() {
-    return service.searchStudentList();
+  public List<StudentDetail> getStudentList() {
+    List<Student> students = service.searchStudentList();
+    List<StudentsCourses> studentsCourses = service.searchStudentsCourseList();
+
+    return converter.convertStudentDetails(students, studentsCourses);
   }
 
-  //	課題用
+
+
+
   @GetMapping("/studentsCourseList")
   public List<StudentsCourses> getStudentsCourseList() {
     return service.searchStudentsCourseList();
